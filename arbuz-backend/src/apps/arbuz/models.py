b8538c71effa4e9a965dd53e1b250ce9a60072ca
charmantemort <dawnwestmorland45@gmail.com>
2025-03-18 18:14:57 +0400

    + model forms

--- arbuz-backend/src/apps/arbuz/models.py
+++ arbuz-backend/src/apps/arbuz/models.py
@@ -2,24 +2,30 @@ from datetime import date, timezone
 from django.db import models
 
 class Projects(models.Model):
+    id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=255)
     description = models.TextField()
     image = models.ImageField()
 
     def __str__(self):
-        return self.name
+        return f"{self.name}"
 
-# CHOICE = (
-#     ('', ''),
-#     ('', '')
-# )
-#
-# # class Forms(models.Model):
-# #     user_name = models.CharField(max_length=255)
-# #     status = models.CharField(max_length=100, choices=CHOICE)
-# #
-# #
-# # class News(models.Model):
-# #     name = models.CharField(max_length=255)
-# #     description = models.TextField()
-# #     created_at = models.DateTimeField(default=timezone.now)
+PAY_CHOICE = (
+    ('разовый', "разовый"),
+    ('ежемесячный', "ежемесячный")
+)
+
+LINKS_CHOICE = (
+    ('да', 'да'),
+    ('нет', 'нет')
+)
+
+class Forms(models.Model):
+    summa = models.DecimalField(decimal_places=1, max_digits=100000)
+    pay_status = models.CharField(max_length=100, choices=PAY_CHOICE)
+    user_name = models.CharField(max_length=255)
+    comment = models.TextField()
+    link_status = models.CharField(max_length=500, choices=LINKS_CHOICE)
+    link_field = models.TextField()
+
+
