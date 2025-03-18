2474935f3e53742c8dfd518c1165c53ffd279204
charmantemort <dawnwestmorland45@gmail.com>
2025-03-18 16:39:43 +0400

    + model projects

--- arbuz-backend/src/arbuz/models.py
+++ arbuz-backend/src/arbuz/models.py
@@ -1,3 +1,10 @@
+from datetime import date, timezone
 from django.db import models
 
-# Create your models here.
+class Projects(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    image = models.ImageField()
+
+    def __str__(self):
+        return self.name
