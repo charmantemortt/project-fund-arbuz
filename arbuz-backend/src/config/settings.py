308ba0abfa16a20cada5af2c9cf2bf150e6be498
charmantemort <dawnwestmorland45@gmail.com>
2025-03-20 13:37:31 +0400

    + fix cors

--- arbuz-backend/src/config/settings.py
+++ arbuz-backend/src/config/settings.py
@@ -53,21 +53,23 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_ORIGINS = [
-    "https://arbuz-backend.santej.space/",
-    "https://walermelon-practic-2.vercel.app/",
-    "http://localhost:8080",
-    "http://127.0.0.1:9000",
+INTERNAL_IPS = [
+    "127.0.0.1",
+    "195.140.146.117"
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://svo.nvkb.ru"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
